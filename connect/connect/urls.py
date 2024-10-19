a7210ecc627e9e55b2cf68ca8692be011d58d4c7
Julscha9291 <j.schaepermeier91@gmail.com>
2024-10-19 19:10:51 +0200

    home template

--- connect/connect/urls.py
+++ connect/connect/urls.py
@@ -1,13 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.views.static import serve  # Hier wird "serve" importiert
 from django.urls import path, include
 from dev.views import custom_404_view  
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('dev.urls')),
+    path('', TemplateView.as_view(template_name='index.html')),  
 ]
 
 handler404 = custom_404_view
