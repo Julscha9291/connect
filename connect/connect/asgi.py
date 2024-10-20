bd15b6573285c3d2c652250bf5a7875f2c3f0af0
Julscha9291 <j.schaepermeier91@gmail.com>
2024-10-20 19:38:24 +0200

    bugs

--- connect/connect/asgi.py
+++ connect/connect/asgi.py
@@ -1,6 +1,9 @@
 import os
-import django
+from django.urls import re_path
 from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
@@ -8,7 +11,6 @@ import dev.routing  # Ersetze durch den Pfad zu deiner Routing-Datei
 from connect.token import TokenAuthMiddleware  # Importiere die TokenAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'connect.settings')
-django_asgi_app = get_asgi_application()
 
 
 application = ProtocolTypeRouter({
@@ -17,7 +19,7 @@ application = ProtocolTypeRouter({
         TokenAuthMiddleware(  # Deine TokenAuthMiddleware
             AuthMiddlewareStack(
                 URLRouter(
-                    dev.routing.websocket_urlpatterns  # Ersetze durch deine WebSocket-URLs
+                    dev.routing.websocket_urlpatterns + [re_path(r"", django_asgi_app)]  # Ersetze durch deine WebSocket-URLs
                 )
             )
         )
