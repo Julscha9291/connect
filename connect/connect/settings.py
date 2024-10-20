db98743d972507697d36ed38ee144ea63d2db7da
Julscha9291 <j.schaepermeier91@gmail.com>
2024-10-20 19:20:17 +0200

    bug fix

--- connect/connect/settings.py
+++ connect/connect/settings.py
@@ -16,14 +16,16 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 from decouple import config
-import django
-django.setup()
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
+
+import django
+django.setup()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
