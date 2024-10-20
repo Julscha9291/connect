b3d67fc0dc2bbb17be24f7947832c6f49689c80c
Julscha9291 <j.schaepermeier91@gmail.com>
2024-10-20 19:17:53 +0200

    django setup

--- connect/connect/settings.py
+++ connect/connect/settings.py
@@ -16,6 +16,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 from decouple import config
+import django
+django.setup()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
