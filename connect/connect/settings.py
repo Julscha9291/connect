4f972fff21d1c3dcd1656e500de3349278910965
Julscha9291 <j.schaepermeier91@gmail.com>
2024-10-23 17:31:51 +0200

    css

--- connect/connect/settings.py
+++ connect/connect/settings.py
@@ -111,7 +111,7 @@ ROOT_URLCONF = 'connect.urls'
 WEBSOCKET_URL = '/ws/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'  
 
